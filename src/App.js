37e9a7f2caa389751f421150a953bf9089e68ddd
Matin Mir <asmamir297@gmail.com>
2024-11-06 14:25:45 +0330

    workbox stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import Dog from "./dog.jpg";
+import "./App.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 function App() {
+  const [waitingWorker, setWaitingWorker] = useState(null);
+
+  useEffect(() => {
+    serviceWorkerRegistration.register({
+      onSuccess: (r) => {
+        setWaitingWorker(r.waiting);
+      },
+    });
+  }, []);
+
+  function activateWorker() {
+    waitingWorker.postMessage({
+      type: "SKIP_WAITING",
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={Dog} alt="Dog" />
+        <p>Hello Darling</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://developer.chrome.com/docs/workbox/modules/workbox-precaching"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sfsdfsdfgfgf
         </a>
       </header>
+      <main>
+        {waitingWorker && (
+          <div>
+            <p>New version is available</p>
+            <button onClick={activateWorker}>Update</button>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// hook.js:608 Error during service worker registration: SecurityError: Failed to register a ServiceWorker for scope ('https://kmstest.irisaco.com/dashboard/') with script ('https://kmstest.irisaco.com/dashboard/service-worker.js'): The script has an unsupported MIME type ('text/html').
